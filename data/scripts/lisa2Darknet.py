2d1e3bd9b0b2831d2f96e60184b22119fa3c12f6
Chase McDermott <chasemcd1745@tamu.edu>
2019-11-18 16:09:21 -0600

    Update LISA script

--- data/scripts/lisa2Darknet.py
+++ data/scripts/lisa2Darknet.py
@@ -13,6 +13,7 @@ NEGATIVE_FILE_PATH = LISA_DOWNLOAD_PATH + 'negatives/negativePics/'
 TRAIN_PERC = .8
 TEST_PERC = 1-TRAIN_PERC
 OUTPUT_IMG_EXTENSION = '.jpg'
+RESIZE_TUPLE = (416, 416)
 
 
 # Saves the image received in the output file path in the OUTPUT_IMG_EXTENSION.
@@ -35,10 +36,10 @@ def calculate_darknet_format(input_img, row):
     image_width = int(real_img_width)
     image_height = int(real_img_height)
 
-    left_x = float(row[2]) / image_width
-    bottom_y = float(row[3]) / image_height
-    right_x = float(row[4]) / image_width
-    top_y = float(row[5]) / image_height
+    left_x = float(row[2])# / image_width
+    bottom_y = float(row[3])# / image_height
+    right_x = float(row[4])# / image_width
+    top_y = float(row[5])# / image_height
 
     object_class = row[1]
 
@@ -60,7 +61,7 @@ def parse_darknet_format(object_class, img_width, img_height, left_x, bottom_y, 
     object_mid_x_rel = object_mid_x / img_width
     object_mid_y_rel = object_mid_y / img_height
 
-    dark_net_label = "{} {} {} {} {}". \
+    dark_net_label = "{0} {1:.10f} {2:.10f} {3:.10f} {4:.10f}". \
         format(object_class, object_mid_x_rel, object_mid_y_rel, object_width_rel, object_height_rel)
 
     return dark_net_label
@@ -86,8 +87,10 @@ for line in gtReader:
     labels[label] += 1
 
     image = Image.open(file_path)
+    # Resize image
+    # resized_img = image.resize(RESIZE_TUPLE)
     darknet_format = calculate_darknet_format(image, line)
-    print(darknet_format)
+    # print(darknet_format)
 
     train_file = rand.choices([True, False], [TRAIN_PERC, TEST_PERC])[0]
 
@@ -108,21 +111,25 @@ with open(LISA_NEW_DATA_PATH + 'lisa.names', 'w+') as f:
 # Move negatives (images without labels) to test and train
 for root, dirs, files in os.walk(NEGATIVE_FILE_PATH):
     for file in files:
-        try:
-            if file.endswith(".png"):
-                image = Image.open(os.path.join(root, file))
-                train_file = rand.choices([True, False], [TRAIN_PERC, TEST_PERC])[0]
-                if train_file:
-                    num_train_files += 1
-                    train_text_file.write(LISA_NEW_DATA_PATH + file.replace('.png', '.jpg'))
-                    image.save(os.path.join(output_train_dir_path, file.replace('.png', '.jpg')))
-
-                else:
-                    num_test_files += 1
-                    test_text_file.write(LISA_NEW_DATA_PATH + file.replace('.png', '.jpg'))
-                    image.save(os.path.join(output_train_dir_path, file.replace('.png', '.jpg')))
-        except:
-            continue
+        # try:
+        if file.endswith(".png"):
+            image = Image.open(os.path.join(root, file))
+            # print(file)
+            # Resize image
+            # resized_img = image.resize(RESIZE_TUPLE)
+            train_file = rand.choices([True, False], [TRAIN_PERC, TEST_PERC])[0]
+            if train_file:
+                num_train_files += 1
+                # train_text_file.write(LISA_NEW_DATA_PATH + file.replace('.png', '.jpg'))
+                # image.save(os.path.join(output_train_dir_path, file.replace('.png', '.jpg')))
+                write_data(file[:-4], image, '', train_text_file, output_train_dir_path)
+            else:
+                num_test_files += 1
+                # test_text_file.write(LISA_NEW_DATA_PATH + file.replace('.png', '.jpg'))
+                # image.save(os.path.join(output_train_dir_path, file.replace('.png', '.jpg')))
+                write_data(file[:-4], image, '', test_text_file, output_test_dir_path)
+        # except:
+            # continue
 
 print('labels: ')
 print(labels)
